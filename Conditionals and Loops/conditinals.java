538e8e325e23bf0d716a7080ca063c4ca323719e
karan <karanc4143@gmail.com>
2024-12-25 18:34:50 +0530

    Add files for conditionals and loops

---
+++ Conditionals and Loops/conditinals.java
@@ -0,0 +1,12 @@
+public class conditinals {
+     public static void main(String [] atgs){
+        int salary =  250000;
+
+        if(salary > 10000){
+            salary = salary +20000;
+        } else{
+            salary = salary + 10000;
+        }
+        System.out.print(salary);
+     }
+}
