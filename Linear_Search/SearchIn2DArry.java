7a0a494bb142f3df76f6c5542a2ceb210db298e7
karan <karanc4143@gmail.com>
2025-01-01 23:55:39 +0530

    2D Search and return index completed

--- Linear_Search/SearchIn2DArry.java
+++ Linear_Search/SearchIn2DArry.java
@@ -1,5 +1,7 @@
 package Linear_Search;
 
+import java.util.Arrays;
+
 public class SearchIn2DArry {
 
     public static void main(String[] args) {
@@ -10,6 +12,8 @@ public class SearchIn2DArry {
         } else {
             System.out.println("oho Sorry it not Present");
         }
+        int[] resultArray = Search(arr, 75);
+        System.out.println(Arrays.toString(resultArray));
     }
 
     static boolean SearchIn2D(int[][] arr, int i) {
@@ -27,4 +31,20 @@ public class SearchIn2DArry {
         return false;
 
     }
+
+    static int[] Search(int[][] arr, int i) {
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] == i) {
+                    return new int[] { row, col };
+                }
+
+            }
+        }
+
+        return new int[] { -1, -1 };
+
+    }
+
 }
