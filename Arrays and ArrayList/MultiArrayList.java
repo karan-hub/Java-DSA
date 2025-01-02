295db3a47714abe51b23463d2f636f94601cfa63
karan <karanc4143@gmail.com>
2025-01-02 16:43:54 +0530

    all done

--- Arrays and ArrayList/MultiArrayList.java
+++ Arrays and ArrayList/MultiArrayList.java
@@ -4,22 +4,23 @@ import java.util.Scanner;
 public class MultiArrayList {
     public static void main(String[] args) {
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        Scanner in = new Scanner(System.in);
-        System.out.println(list);
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println(list);
 
 
-        for (int index = 0; index < 3; index++) {
-            list.add(new ArrayList<>(3));
-        }
-        System.out.println(list.size());
-        
+            for (int index = 0; index < 3; index++) {
+                list.add(new ArrayList<>(3));
+            }
+            System.out.println(list.size());
+            
 
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i).size());
-            for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(list.get(i).size());
+                for (int j = 0; j < 3; j++) {
 
-                list.get(i).add(in.nextInt());
-                
+                    list.get(i).add(in.nextInt());
+                    
+                }
             }
         }
         System.out.println(list);
