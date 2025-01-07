73d92216dc890166032edc8a15f72bd7bcb5efa7
karan <karanc4143@gmail.com>
2025-01-08 01:03:09 +0530

    Add IfElse class to compare two integers and return comparison result

---
+++ Step 1 : Learn the basics/IfElse.java
@@ -0,0 +1,21 @@
+// package Step 1 : Learn the basics;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int n = 5;
+        int m = 10;
+        String result = compareNM(n, m);
+        System.out.println("The comparison result is: " + result);
+    }
+    public static String compareNM(int n, int m) {
+        if(n==m){
+            return "equal" ;
+        }else if(n<m){
+            return "lesser" ;
+        }else{
+            return "greater" ;
+        }
+        
+        // code here
+    }
+}
