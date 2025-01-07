d0379d7697acc29688144c074d847f324c478045
karan <karanc4143@gmail.com>
2025-01-08 02:02:28 +0530

    Add Whileloops class to demonstrate multiplication table using a while loop

---
+++ Step 1 : Learn the basics/Whileloops.java
@@ -0,0 +1,13 @@
+package Step 1 : Learn the basics;
+
+public class Whileloops {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);;
+        int table = sc.nextInt();
+        int i =10 ;
+        while( i >= 1){
+            System.out.print(table * i + " ") ;
+            i--;
+        }
+    }
+}
