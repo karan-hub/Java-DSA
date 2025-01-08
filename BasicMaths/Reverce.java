72c8b5e79a53154907bda7b06946482e51056e76
karan <karanc4143@gmail.com>
2025-01-08 15:11:33 +0530

    Add Reverce class to reverse an integer and handle negative values

---
+++ BasicMaths/Reverce.java
@@ -0,0 +1,26 @@
+package BasicMaths;
+
+public class Reverce {
+    public static void main(String[] args) {
+        Reverce rev = new Reverce();
+        int number = -12345;
+        int reversedNumber = rev.reverse(number);
+        System.out.println("Reversed number: " + reversedNumber);
+    }
+    public int reverse(int x) {
+        int result =0;
+        // int sign = x < 0 ? -1:1 ;
+         int sign = x < 0 ? -1 : 1;
+z        while(x !=0){
+
+            int remender = x % 10 ;
+            x /=10 ;
+
+
+            result = result *10 + remender ;
+
+        }
+        return result * sign ;
+
+    }
+}
