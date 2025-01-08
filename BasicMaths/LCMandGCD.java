e8c3cd273534ad5f8ead71c148e499e392459f57
karan <karanc4143@gmail.com>
2025-01-08 19:00:05 +0530

    Add LCMandGCD class to calculate GCD using both better and optimal approaches

---
+++ BasicMaths/LCMandGCD.java
@@ -0,0 +1,44 @@
+package BasicMaths;
+
+public class LCMandGCD {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 18;
+
+        int gcdBetter = BetterApproach(a, b);
+        System.out.println("GCD using Better Approach: " + gcdBetter);
+
+        int gcdOptimal = OptimalApproch(a, b);
+        System.out.println("GCD using Optimal Approach: " + gcdOptimal);
+
+    }
+
+    public static int BetterApproach(int a, int b) {
+
+        int result = 0;
+        int min = Math.min(a, b);
+        for (int i = 1; i <= min; i++) {
+            if (a % i == 0 && b % i == 0) {
+                result = i;
+            }
+        }
+        return result;
+    }
+
+    public static int OptimalApproch(int a, int b) {
+        while (a != 0 && b != 0) {
+            int max = Math.max(a, b);
+            int min = Math.min(a, b);
+
+            int sub = max - min;
+            a = min;
+            b = sub;
+            if (sub == 0) {
+                return min;
+            }
+
+        }
+        return 0;
+    }
+
+}
