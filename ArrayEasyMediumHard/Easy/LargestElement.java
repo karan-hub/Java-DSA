495eb894620bf18ae558afaf924e4da2fb2bd6bb
karan <karanc4143@gmail.com>
2025-01-10 14:05:20 +0530

    Add BruteForce2 method for finding the largest element and create SecondLargest class

--- ArrayEasyMediumHard/Easy/LargestElement.java
+++ ArrayEasyMediumHard/Easy/LargestElement.java
@@ -1,10 +1,12 @@
 package ArrayEasyMediumHard.Easy;
 
+import java.util.Arrays;
+
  
 public class LargestElement {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
-        System.out.println(BruteForce(arr));
+        System.out.println(BruteForce2(arr));
     }
     
     public static int BruteForce(int[] arr) {
@@ -16,5 +18,15 @@ public class LargestElement {
         }
         return max;
         
+    }
+
+
+    public static int  BruteForce2(int[] arr){
+
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        return arr[arr.length-1];
+
     }
 }
---
+++ ArrayEasyMediumHard/Easy/SecondLargest.java
@@ -0,0 +1,14 @@
+package ArrayEasyMediumHard.Easy;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int getSecondLargest(int[] arr) {
+        // Code Here
+        
+
+        return 0;
+    }
+}
