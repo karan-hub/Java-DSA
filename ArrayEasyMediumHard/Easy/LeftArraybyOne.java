cf6234fb8c5f839a23e6377e51198ceae5ab13a1
karan <karanc4143@gmail.com>
2025-01-11 15:37:02 +0530

    Refactor LeftArraybyOne class to replace brute force method with an optimal solution for rotating an array left by one position

--- ArrayEasyMediumHard/Easy/LeftArraybyOne.java
+++ ArrayEasyMediumHard/Easy/LeftArraybyOne.java
@@ -4,7 +4,7 @@ public class LeftArraybyOne {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5};
         System.out.println(nums.length);
-        int[] result = BruteForce(nums);
+        int[] result = Optimal(nums);
         for (int num : result) {
             System.out.print(num + " ");
         }
@@ -21,4 +21,16 @@ public class LeftArraybyOne {
         temp[nums.length-1]= frist;
         return temp;
     }
+
+    public static int[] Optimal(int [] nums){
+        int temp = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i-1]=nums[i];
+        }
+        nums[nums.length-1]=temp;
+
+        return nums;
+
+    }
+
 }
