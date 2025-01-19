4d675116b0821be23070d47f097b765e268857b9
karan <karanc4143@gmail.com>
2025-01-19 23:19:57 +0530

    Refactor FindMissing class to improve readability and rename missing method to missingBrutForce1

--- ArrayEasyMediumHard/Easy/FindMissing.java
+++ ArrayEasyMediumHard/Easy/FindMissing.java
@@ -3,18 +3,17 @@ package ArrayEasyMediumHard.Easy;
 class FindMissing {
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 4, 5, 6};
+        int[] arr = { 1, 2, 4, 5, 6 };
 
-        
         int missingNumber = missing(arr);
         System.out.println("The missing number is: " + missingNumber);
     }
 
-    public static int missing(int[] arr) {
+    // brut force
+    public static int missingBrutForce1(int[] arr) {
 
         for (int i = 1; i < arr.length; i++) {
             int flag = 0;
-
 
             for (int j = 0; j < arr.length; j++) {
                 if (arr[j] == i) {
@@ -22,10 +21,9 @@ class FindMissing {
                     break;
                 }
 
-                
             }
             if (flag == 0)
-                    return i;
+                return i;
         }
 
         return -1;
