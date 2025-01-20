30010ee405fff819168073f839c78f5729146ea0
karan <karanc4143@gmail.com>
2025-01-20 19:22:09 +0530

    Add SumOfSubArray class to calculate the length of subarrays with a given target sum

---
+++ ArrayEasyMediumHard/Easy/SumOfSubArray.java
@@ -0,0 +1,63 @@
+package ArrayEasyMediumHard.Easy;
+
+public class SumOfSubArray {
+    public static void main(String[] args) {
+        int[] arr = {2,3,5};
+        int targetSum = 5;
+        int result = usingBrutforce1(arr, targetSum);
+        System.out.println("Length of subarray with target sum: " + result);
+    }
+    // using two pointers
+
+    public static int lenghtOfSubarray(int[] arr, int targetSum) {
+
+        int Start = 0;
+        int end = 0;
+        int currenSum = 0;
+        int maxLength = 0;
+        while (end < arr.length) {
+            currenSum += arr[end];
+
+            while (currenSum > targetSum) {
+                currenSum -= arr[Start];
+                Start++;
+            }
+
+            if (currenSum == targetSum) {
+                maxLength = Math.max(maxLength, end - Start + 1);
+            }
+            end++;
+        }
+        return maxLength;
+
+    }
+
+    public static int usingBrutforce1(int[] num, int targetSum) {
+        int len = 0;
+        for (int i = 0; i < num.length; i++) {
+            for (int j = i; j < num.length; j++) {
+                int sum =SumSrartToEnd(num, i, j);
+                if (targetSum == sum) {
+                    len = Math.max(len, j-i + 1);
+                }
+
+            }
+        }
+
+        return len;
+    }
+
+    public static int SumSrartToEnd(int[] num, int startIndex, int endIndex) {
+
+        if (startIndex > num.length && endIndex > num.length) {
+            return -1;
+        }
+
+        int sum = 0;
+
+        for (int i = startIndex; i <= endIndex; i++) {
+            sum = sum + num[i];
+        }
+        return sum;
+    }
+}
