b0947bf501302e69907fe3e1fc4ec0d3f35514f5
karan <karanc4143@gmail.com>
2025-01-21 13:20:38 +0530

    Add TwosSumProblem class to implement a solution for the two-sum problem

---
+++ ArrayEasyMediumHard/Medium/TwosSumProblem.java
@@ -0,0 +1,26 @@
+package ArrayEasyMediumHard.Medium;
+
+public class TwosSumProblem {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        towSum(nums);
+    }
+
+    public static int[] towSum(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i; j < nums.length; j++) {
+                     
+            }
+        }
+
+        return new int[] { -1, -1 };
+
+    }
+
+
+    
+
+}
