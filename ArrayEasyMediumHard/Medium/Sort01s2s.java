713b51e33b2bbf60fdd4b44b6b0cc50831c40282
karan <karanc4143@gmail.com>
2025-01-21 16:51:54 +0530

    Add BatterApproch method to Sort01s2s class for sorting an array of 0s, 1s, and 2s

--- ArrayEasyMediumHard/Medium/Sort01s2s.java
+++ ArrayEasyMediumHard/Medium/Sort01s2s.java
@@ -2,8 +2,9 @@ package ArrayEasyMediumHard.Medium;
 
 public class Sort01s2s {
     public static void main(String[] args) {
-        int[] result = swapThreeNumbers(2,2,1);
-        for (int num : result) {
+        int[] nums = {2, 0, 1, 2, 0, 1, 2, 0, 1};
+        int[] sortedNums = BatterApproch(nums);
+        for (int num : sortedNums) {
             System.out.print(num + " ");
         }
     }
@@ -27,5 +28,41 @@ public class Sort01s2s {
         }
 
         return new int[] { a, b, c };
+    }
+
+    public static int[] BatterApproch(int[] nums) {
+
+        int i = 0;
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+        while (i < nums.length) {
+            if (nums[i] == 0) {
+                zero++;
+            }
+            if (nums[i] == 1) {
+                one++;
+            }
+            if (nums[i] == 2) {
+                two++;
+            }
+            i++;
+        }
+
+        for (int j = 0; j < zero; j++) {
+            nums[j]=0;
+
+        }
+        for (int j = zero; j < zero + one; j++) {
+            nums[j]=1;
+
+        }
+        for (int j = zero + one; j < nums.length; j++) {
+            nums[j]=2;
+
+        }
+
+        return nums;
+
     }
 }
