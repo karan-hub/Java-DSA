06a6a856e661561757d47841d0428fb69a88dd9e
karan <karanc4143@gmail.com>
2025-01-21 13:32:00 +0530

    Refactor TwosSumProblem class to add target sum parameter and update main method for testing

--- ArrayEasyMediumHard/Medium/TwosSumProblem.java
+++ ArrayEasyMediumHard/Medium/TwosSumProblem.java
@@ -3,23 +3,30 @@ package ArrayEasyMediumHard.Medium;
 public class TwosSumProblem {
 
     public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        towSum(nums);
+        int[] nums = { 21, 7, 12, 2 };
+        int targetSum = 9;
+        int[] result = towSum(nums, targetSum);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
     }
 
-    public static int[] towSum(int[] nums) {
+    public static int[] towSum(int[] nums, int targetSum) {
 
         for (int i = 0; i < nums.length; i++) {
 
             for (int j = i; j < nums.length; j++) {
-                     
+                int test = nums[i] + nums[j];
+                // System.out.println(nums[i] + " + " + nums[j] + " = " + test);
+
+               
+                if (test == targetSum) {
+                    return new int[] { i, j };
+                }
             }
         }
 
         return new int[] { -1, -1 };
 
     }
-
 
     
 
