dbf44d410c7b5d65f15fc72e8c44fa06bdcb7484
karan <karanc4143@gmail.com>
2025-01-21 15:37:22 +0530

    Refactor TwosSumProblem class to implement two-pointer approach and add saveIndex method for index tracking

--- ArrayEasyMediumHard/Medium/TwosSumProblem.java
+++ ArrayEasyMediumHard/Medium/TwosSumProblem.java
@@ -1,24 +1,28 @@
 package ArrayEasyMediumHard.Medium;
 
+import java.util.Arrays;
+
 public class TwosSumProblem {
 
     public static void main(String[] args) {
-        int[] nums = { 21, 7, 12, 2 };
-        int targetSum = 9;
-        int[] result = towSum(nums, targetSum);
-        System.out.println("Indices: " + result[0] + ", " + result[1]);
+        int[] nums = { 3, 3, 12, 2 };
+        int targetSum = 6;
+
+        saveIndex(nums);
+
+        int[] result2 = usingTwoPointers(nums, targetSum);
+        System.out.println("Indices from usingTwoPointers: " + result2[0] + ", " + result2[1]);
+
     }
 
-    public static int[] towSum(int[] nums, int targetSum) {
+    public static int[] towSum1(int[] nums, int targetSum) {
 
         for (int i = 0; i < nums.length; i++) {
 
-            for (int j = i; j < nums.length; j++) {
-                int test = nums[i] + nums[j];
+            for (int j = i + 1; j < nums.length; j++) {
                 // System.out.println(nums[i] + " + " + nums[j] + " = " + test);
 
-               
-                if (test == targetSum) {
+                if (nums[i] + nums[j] == targetSum) {
                     return new int[] { i, j };
                 }
             }
@@ -28,6 +32,41 @@ public class TwosSumProblem {
 
     }
 
-    
+    public static int[] usingTwoPointers(int[] nums, int targetSum) {
+
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+
+        int Start = 0;
+        int end = nums.length - 1;
+
+        while (Start < end) {
+            int sum = nums[Start] + nums[end];
+
+            if (sum == targetSum) {
+                return new int[] { Start, end };
+            } else if (sum > targetSum) {
+                end--;
+            } else {
+                Start++;
+            }
+        }
+        return new int[] { -1, -1 };
+
+    }
+
+    public static void saveIndex(int[] nums) {
+        int len = nums.length;
+        int[][] saveIndexandValue = new int[len][2];
+
+         for (int i = 0; i < nums.length; i++) {
+            saveIndexandValue[i][0]=nums[i];
+            saveIndexandValue[i][1]=i;
+         }
+         Arrays.sort(saveIndexandValue, (a, b) -> Integer.compare(a[1], b[1]));
+         System.out.println(Arrays.deepToString(saveIndexandValue) +" jvgv ");
+
+
+    }
 
 }
