6ff9169c2bf1abd41f5fb45eb0c62fb8773e5b8b
karan <karanc4143@gmail.com>
2025-01-23 21:47:44 +0530

    Add OptimalApproch method to LongestSubarray class for calculating the sum of the longest subarray

--- ArrayEasyMediumHard/Medium/LongestSubarray.java
+++ ArrayEasyMediumHard/Medium/LongestSubarray.java
@@ -2,9 +2,11 @@ package ArrayEasyMediumHard.Medium;
 
 public class LongestSubarray {
     public static void main(String[] args) {
-        int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
-        int result = subArray(arr);
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int result = OptimalApproch(arr);
         System.out.println("The sum of the longest subarray is: " + result);
+        // // int y = usingwhileloop(arr);
+        // System.out.println(y);
 
     }
 
@@ -13,7 +15,7 @@ public class LongestSubarray {
         for (int start = 0; start < arr.length; start++) {
             for (int end = 0; end < arr.length; end++) {
                 int curretnSum = 0;
-                for (int i = start; i <=end; i++) {
+                for (int i = start; i <= end; i++) {
                     curretnSum += arr[i];
                 }
                 if (sum < curretnSum) {
@@ -23,6 +25,25 @@ public class LongestSubarray {
             }
         }
         return sum;
+
+    }
+
+    public static int OptimalApproch(int array[]) {
+
+        int sum = 0;
+        int curretnSum = Integer.MIN_VALUE;
+
+        for (int num : array) {
+
+            sum += num;
+            if (sum > curretnSum) {
+                curretnSum = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return curretnSum;
 
     }
 }
