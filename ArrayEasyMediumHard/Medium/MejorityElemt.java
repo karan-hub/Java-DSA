4e8ae0d1a7bf1cef913f539faa112b5e0e171981
karan <karanc4143@gmail.com>
2025-01-23 17:21:29 +0530

    Add BatterApproch method to MejorityElemt class for finding the majority element using sorting

--- ArrayEasyMediumHard/Medium/MejorityElemt.java
+++ ArrayEasyMediumHard/Medium/MejorityElemt.java
@@ -1,10 +1,12 @@
 package ArrayEasyMediumHard.Medium;
 
+import java.util.Arrays;
+
 public class MejorityElemt {
 
     public static void main(String[] args) {
-        int[] nums = {  1,2,2,1,1};
-        int result = Mejority(nums);
+        int[] nums = { 1, 2, 2, 1, 1 };
+        int result = BatterApproch(nums);
         System.out.println("The majority element is: " + result);
 
     }
@@ -27,6 +29,28 @@ public class MejorityElemt {
             }
         }
         return ans;
+
+    }
+
+    public static int BatterApproch(int[] nums) {
+
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        int ans = nums[0];
+        int frequency = 0;
+        for (int i : nums) {
+            if (i == ans) {
+                frequency++;
+            } else {
+                ans = i;
+            }
+
+            if (frequency == Math.floorDiv(nums.length, 2)) {
+                return ans;
+            }
+        }
+
+        return -1;
 
     }
 }
