85e103980876608fb59912294e78c27dce0943d8
karan <karanc4143@gmail.com>
2025-01-26 02:39:14 +0530

    Add StockBuyAndSell class to calculate maximum profit from stock prices

---
+++ ArrayEasyMediumHard/Medium/StockBuyAndSell.java
@@ -0,0 +1,24 @@
+package ArrayEasyMediumHard.Medium;
+
+public class StockBuyAndSell {
+    public static void main(String[] args) {
+        int[] prices = {7, 2, 5, 3, 6, 4 ,1,20,23 };
+        System.out.println("Maximum profit: " + Profit(prices));
+
+    }
+
+    public static int Profit(int[] prices) {
+
+        int maxProfit = 0, buyStock = prices[0];
+        for (int price : prices) {
+            if (price > buyStock) {
+                maxProfit = Math.max(maxProfit, price - buyStock);
+            }
+
+            buyStock = Math.min(buyStock, price);
+        }
+
+        return maxProfit;
+
+    }
+}
