7c406c8c5ff3d41937128165b566738413d06dce
karan <karanc4143@gmail.com>
2025-01-26 01:12:11 +0530

    Refactor MaximumScoreFromSubarrayMinimums class for improved readability and maintainability; adjust variable formatting and remove commented code

--- ArrayEasyMediumHard/Medium/MaximumScoreFromSubarrayMinimums.java
+++ ArrayEasyMediumHard/Medium/MaximumScoreFromSubarrayMinimums.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class MaximumScoreFromSubarrayMinimums {
     public static void main(String[] args) {
-        int[] nums = {4, 3, 1, 5, 6};
-        int x=minimumSubArray(nums);
+        int[] nums = { 4, 3, 1, 5, 6 };
+        int x = minimumSubArray(nums);
         System.out.println(x);
 
     }
@@ -14,11 +14,11 @@ public class MaximumScoreFromSubarrayMinimums {
         int small = Integer.MAX_VALUE;
         int secondSmall = Integer.MAX_VALUE;
 
-        for (int i = Start; i <=End; i++) {
+        for (int i = Start; i <= End; i++) {
             int current = nums[i];
             if (current < small) {
-                secondSmall = small; //big , 4 , 3
-                small = current; //4 , 3 , 1
+                secondSmall = small; // big , 4 , 3
+                small = current; // 4 , 3 , 1
             }
             if (current > small && current < secondSmall) {
                 secondSmall = current;
@@ -33,10 +33,23 @@ public class MaximumScoreFromSubarrayMinimums {
         int currentMax = 0;
 
         for (int start = 0; start < nums.length; start++) {
+            int small = Integer.MAX_VALUE;
+            int secondSmall = Integer.MAX_VALUE;
 
             for (int end = start; end < nums.length; end++) {
-                int result[] = smallandsecsmall(nums, start, end);
-                int max = result[0] + result[1];
+                int current = nums[end];
+
+                if (current < small) {
+                    secondSmall = small; // big , 4 , 3
+                    small = current; // 4 , 3 , 1
+                }
+                if (current > small && current < secondSmall) {
+                    secondSmall = current;
+                }
+
+                // int result[] = smallandsecsmall(nums, start, end);
+                // int max = result[0] + result[1];
+                int max = small + secondSmall;
                 if (currentMax < max) {
                     currentMax = max;
                 }
