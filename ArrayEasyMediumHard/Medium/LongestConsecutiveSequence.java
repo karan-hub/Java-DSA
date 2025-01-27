0ad6d441e47091ef69f1db780269e6927fe71612
karan <karanc4143@gmail.com>
2025-01-28 00:49:23 +0530

    Add batterApproch method to LongestConsecutiveSequence class for finding longest consecutive sequence

--- ArrayEasyMediumHard/Medium/LongestConsecutiveSequence.java
+++ ArrayEasyMediumHard/Medium/LongestConsecutiveSequence.java
@@ -1,18 +1,20 @@
 package ArrayEasyMediumHard.Medium;
 
+import java.util.Arrays;
+
 public class LongestConsecutiveSequence {
     public static void main(String[] args) {
 
         int[] testArray1 = { 3, 8, 5, 7, 6 };
-        int result1 = Sequence(testArray1);
+        int result1 = batterApproch(testArray1);
         System.out.println("Test 1 - Expected: 4, Actual: " + result1);
 
         int[] testArray2 = { 1, 2, 0, 1 };
-        int result2 = Sequence(testArray2);
+        int result2 = batterApproch(testArray2);
         System.out.println("Test 2 - Expected: 3, Actual: " + result2);
 
         int[] testArray3 = { 100, 4, 200, 1, 3, 2 };
-        int result3 = Sequence(testArray3);
+        int result3 = batterApproch(testArray3);
         System.out.println("Test 3 - Expected: 4, Actual: " + result3);
     }
 
@@ -40,4 +42,24 @@ public class LongestConsecutiveSequence {
         }
         return false;
     }
+
+    public static int batterApproch(int[] nums) {
+
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        int lastIndex = Integer.MIN_VALUE, count = 0, longest = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] - 1 == lastIndex) {
+                count++;
+                lastIndex = nums[i];
+            } else if (nums[i] != lastIndex) {
+                count = 1;
+                lastIndex = nums[i];
+            }
+            longest = Math.max(longest, count);
+        }
+        return longest;
+    }
+
 }
