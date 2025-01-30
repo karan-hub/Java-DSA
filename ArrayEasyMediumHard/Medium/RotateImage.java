8d72e80a45b13abc164b1884ee1211f5017c6d91
karan <karanc4143@gmail.com>
2025-01-31 03:09:06 +0530

    Add matrix initialization and output in RotateImage main method

--- ArrayEasyMediumHard/Medium/RotateImage.java
+++ ArrayEasyMediumHard/Medium/RotateImage.java
@@ -2,7 +2,20 @@ package ArrayEasyMediumHard.Medium;
 
 public class RotateImage {
     public static void main(String[] args) {
-        
+        int[][] matrix = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+
+        rotate(matrix);
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
 
     }
 
