700b381c4ea87b0c3b4d09b5e50e8cfa5e7cf52e
karan <karanc4143@gmail.com>
2025-02-01 19:01:13 +0530

    batter Solutation O(n)^2

--- ArrayEasyMediumHard/Medium/CountSubarray.java
+++ ArrayEasyMediumHard/Medium/CountSubarray.java
@@ -7,7 +7,7 @@ public class CountSubarray {
         int[] nums = { 3, 1, 2, 4 };
         int k = 6;
 
-        System.out.println(brutForce(nums, k));
+        System.out.println(batterSolutation(nums, k));
 
     }
 
@@ -29,5 +29,23 @@ public class CountSubarray {
 
         return count;
 
+    }
+
+    public static int batterSolutation(int[] nums, int n) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+
+                if (sum == n) {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return count;
     }
 }
