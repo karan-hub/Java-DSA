7cdafd98f22127ca145f91cfc74e13f13918a17b
karan <karanc4143@gmail.com>
2025-02-02 18:53:32 +0530

    this is optimal approch

--- ArrayEasyMediumHard/Hard/MajorityElementII.java
+++ ArrayEasyMediumHard/Hard/MajorityElementII.java
@@ -9,7 +9,7 @@ public class MajorityElementII {
 
     public static void main(String[] args) {
         int[] arr = { 11, 33, 33, 11, 33, 11 };
-        List<Integer> ans = batterApproch(arr);
+        List<Integer> ans = optimaApproch(arr);
         System.out.print("The majority elements are: ");
         for (int i = 0; i < ans.size(); i++) {
             System.out.print(ans.get(i) + " ");
@@ -72,6 +72,46 @@ public class MajorityElementII {
     // Boyer Moore's Voting Algorithm
 
     public static List<Integer> optimaApproch(int[] nums) {
+        int n = nums.length;
+        long count1 = 0, count2 = 0;
+        Long element1 = null, element2 = null;
+        List<Integer> list = new ArrayList<>();
+
+        for (int num : nums) {
+            if (element1 != null && num == element1) {
+                count1++;
+            } else if (element2 != null && num == element2) {
+                count2++;
+            } else if (count1 == 0) {
+                element1 = (long) num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                element2 = (long) num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Validate the counts
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (element1 != null && num == element1)
+                count1++;
+            if (element2 != null && num == element2)
+                count2++;
+        }
+
+        int mini = n / 3;
+        if (element1 != null && count1 > mini)
+            list.add(element1.intValue());
+        if (element2 != null && count2 > mini)
+            list.add(element2.intValue());
+
+        Collections.sort(list);
+        return list;
 
     }
 
