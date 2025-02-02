04d704f15bc5e1ef7542c9144e5664a674e887e3
karan <karanc4143@gmail.com>
2025-02-02 18:26:49 +0530

    done

--- ArrayEasyMediumHard/Hard/MajorityElementII.java
+++ ArrayEasyMediumHard/Hard/MajorityElementII.java
@@ -1,13 +1,15 @@
 package ArrayEasyMediumHard.Hard;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class MajorityElementII {
 
     public static void main(String[] args) {
         int[] arr = { 11, 33, 33, 11, 33, 11 };
-        List<Integer> ans = brutForce(arr);
+        List<Integer> ans = batterApproch(arr);
         System.out.print("The majority elements are: ");
         for (int i = 0; i < ans.size(); i++) {
             System.out.print(ans.get(i) + " ");
@@ -44,4 +46,33 @@ public class MajorityElementII {
 
         return list;
     }
+
+    public static List<Integer> batterApproch(int[] nums) {
+
+        List<Integer> list = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int minValue = (int) (nums.length / 3) + 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            int value = map.getOrDefault(nums[i], 0);
+            map.put(nums[i], value + 1);
+
+            if (map.get(nums[i]) == minValue) {
+                list.add(nums[i]);
+            }
+            if (list.size() == 2) {
+                break;
+            }
+
+        }
+        Collections.sort(list);
+        return list;
+    }
+
+    // Boyer Moore's Voting Algorithm
+
+    public static List<Integer> optimaApproch(int[] nums) {
+
+    }
+
 }
