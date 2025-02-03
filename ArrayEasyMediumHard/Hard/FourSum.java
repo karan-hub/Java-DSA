c112000934634042d7fbb9e03519f7a5f97640f0
karan <karanc4143@gmail.com>
2025-02-04 00:33:16 +0530

    batter Approch

--- ArrayEasyMediumHard/Hard/FourSum.java
+++ ArrayEasyMediumHard/Hard/FourSum.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class FourSum {
     public static void main(String[] args) {
 
-        int[] nums = { -1, 0, 1, 2,  -1, -4 };
-         Set<List<Integer>> ans = brutForce(nums ,0);
+        int[] nums = { -1, 0, 1, 2, -1, -4 };
+        Set<List<Integer>> ans = batterApproch(nums, 0);
         for (List<Integer> list : ans) {
             System.out.println("result" + list.toString());
         }
@@ -37,6 +37,30 @@ public class FourSum {
 
     }
 
+    public static Set<List<Integer>> batterApproch(int[] nums, int target) {
 
-    
+        Set<List<Integer>> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                Set<Integer> map = new HashSet<>();
+                for (int k = j + 1; k < nums.length; k++) { 
+                    int sum = nums[i] + nums[k] + nums[j];   
+                    int L = target - sum;
+                    if (map.contains(L)) {
+                        List<Integer> list = new ArrayList<>();
+                        list.addAll(List.of(nums[i], nums[j], nums[k], L));
+                        Collections.sort(list);
+                        set.add(list);
+
+                    }
+                    map.add(nums[k]);
+                }
+            }
+        }
+
+        return set;
+
+    }
+
 }
