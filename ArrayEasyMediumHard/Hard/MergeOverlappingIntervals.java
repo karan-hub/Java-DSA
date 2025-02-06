0e7cf4bc784ac645ead0448dd8dde1a8e87bc11d
karan <karanc4143@gmail.com>
2025-02-06 13:17:52 +0530

    every thinf is up to date

--- ArrayEasyMediumHard/Hard/MergeOverlappingIntervals.java
+++ ArrayEasyMediumHard/Hard/MergeOverlappingIntervals.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class MergeOverlappingIntervals {
     public static void main(String[] args) {
         int[][] arr = { { 1, 3 }, { 8, 10 }, { 2, 6 }, { 15, 18 } };
-        List<List<Integer>> ans = brutForce(arr);
+        List<List<Integer>> ans = optimalSolution(arr);
         System.out.print("The merged intervals are: \n");
         for (List<Integer> it : ans) {
             System.out.print("[" + it.get(0) + ", " + it.get(1) + "] ");
@@ -52,8 +52,8 @@ public class MergeOverlappingIntervals {
                         .boxed()
                         .toArray(Integer[]::new))));
 
-            }else{
-                
+            } else {
+                ans.get(ans.size() - 1).set(1, Math.max(ans.get(ans.size() - 1).get(1), nums[i][1]));
             }
         }
 
