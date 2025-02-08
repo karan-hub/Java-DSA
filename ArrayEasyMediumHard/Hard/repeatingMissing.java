2fc4271f02e7164daa985f77083efbf0c69997d2
karan <karanc4143@gmail.com>
2025-02-08 17:38:09 +0530

    batter approch

--- ArrayEasyMediumHard/Hard/repeatingMissing.java
+++ ArrayEasyMediumHard/Hard/repeatingMissing.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class repeatingMissing {
     public static void main(String[] args) {
         int[] nums = new int[] { 3, 1, 2, 5, 3 };
-        System.out.println(Arrays.toString(brutforce(nums)));
+        System.out.println(Arrays.toString(batterApproch(nums)));
     }
 
     public static int[] brutforce(int[] nums) {
@@ -28,6 +28,27 @@ public class repeatingMissing {
 
             if (missing != -1 && repiting != -1) {
                 return new int[] { missing, repiting };
+            }
+
+        }
+
+        return new int[] { missing, repiting };
+    }
+
+    public static int[] batterApproch(int[] nums) {
+        int[] hashArray = new int[nums.length + 1];
+        for (int num : nums) {
+            hashArray[num]++;
+        }
+        System.out.println(Arrays.toString(hashArray));
+
+        int missing = -1, repiting = -1;
+
+        for (int i = 1; i < hashArray.length; i++) {
+            if (hashArray[i] == 0) {
+                missing = i;
+            } else if (hashArray[i] == 2) {
+                repiting = i;
             }
 
         }
