623a59c966b9f5a5159a8c6ec36dfad195306320
karan <karanc4143@gmail.com>
2025-03-25 22:04:47 +0530

    Refactor Median1 class to update test cases and improve median calculation logic

--- BinarySearchEasyMediunHard/Easy/Median1.java
+++ BinarySearchEasyMediunHard/Easy/Median1.java
@@ -3,16 +3,12 @@ package BinarySearchEasyMediunHard.Easy;
 public class Median1 {
     public static void main(String[] args) {
 
-        int[] nums1 = { 1, 3 };
-        int[] nums2 = { 2 };
+        int[] nums1 = { 1, 2 };
+        int[] nums2 = { 3, 4 };
         double median = findMedianSortedArrays(nums1, nums2);
         System.out.println("The median is: " + median);
         // Verify the result with the correct answer
-        if (median == 2.0) {
-            System.out.println("The answer is correct.");
-        } else {
-            System.out.println("The answer is incorrect.");
-        }
+
     }
 
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
@@ -24,7 +20,7 @@ public class Median1 {
         int currentIndex = 0;
         int element1 = -1, element2 = -1;
 
-        while (i < length1 && j < length2) {
+        while (i < length1 - 1 && j < length2 - 1) {
             if (nums1[i] < nums2[j]) {
 
                 if (firstMid == currentIndex)
@@ -45,14 +41,27 @@ public class Median1 {
             }
 
         }
-        if (element1 != -1 && element2 != -1) {
-            if (finalLenght % 2 == 1)
-                return element1;
-            else
-                return (double) (element1 + element2) / 2;
+        while (i < length1) {
+            if (firstMid == currentIndex)
+                element1 = nums1[i];
+            if (secondMid == currentIndex)
+                element2 = nums1[i];
+            currentIndex++;
+            i++;
+        }
+        while (j < element2) {
+            if (firstMid == currentIndex)
+                element1 = nums2[j];
+            if (secondMid == currentIndex)
+                element2 = nums2[j];
+            currentIndex++;
+            j++;
         }
 
-        return 0;
+        if (finalLenght % 2 == 1)
+            return element1;
+
+        return (double) (element1 + element2) / 2;
 
     }
 }
