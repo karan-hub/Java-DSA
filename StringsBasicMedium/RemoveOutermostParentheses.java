e4cd14b6a6d405c21ac5586efe75a92b810aa253
karan <karanc4143@gmail.com>
2025-04-05 22:22:00 +0530

    🐍 Updated tringsBasicMedium/RemoveOutermostParentheses.java

--- StringsBasicMedium/RemoveOutermostParentheses.java
+++ StringsBasicMedium/RemoveOutermostParentheses.java
@@ -1,11 +1,16 @@
 package StringsBasicMedium;
 
+import java.util.Stack;
+
 public class RemoveOutermostParentheses {
     public static void main(String[] args) {
         RemoveOutermostParentheses obj = new RemoveOutermostParentheses();
         String input = "(()())(())";
         String result = obj.removeOuterParentheses(input);
         System.out.println("Result: " + result);
+        String resultStack = obj.removeOuterParenthesesstack(input);
+        System.out.println("Result using stack: " + resultStack);
+
     }
     public String removeOuterParentheses(String s) {
         int count =0 ;
@@ -14,6 +19,23 @@ public class RemoveOutermostParentheses {
             if(s.charAt(i) == ')') count --;
             if(count !=0 ) ans+=s.charAt(i);
             if(s.charAt(i) == '(') count ++;
+
+        }
+        return ans;
+    }
+    public String removeOuterParenthesesstack(String s) {
+        Stack<Character> st = new Stack<>();
+        String ans="";
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                if (!st.isEmpty()) ans+=s.charAt(i);
+                st.push(s.charAt(i));
+            }
+            if(s.charAt(i) == ')') {
+                st.pop();
+                if (!st.isEmpty()) ans+=s.charAt(i);
+            }
+            
 
         }
         return ans;
