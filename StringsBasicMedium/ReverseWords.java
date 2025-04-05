ac482d04b18cd76c31e91a35835b674cc0b105c5
karan <karanc4143@gmail.com>
2025-04-05 23:32:23 +0530

    🛠️ Modified tringsBasicMedium/ReverseWords.java

--- StringsBasicMedium/ReverseWords.java
+++ StringsBasicMedium/ReverseWords.java
@@ -36,4 +36,14 @@ public class ReverseWords {
         }
         return ans.toString();
     }
+
+
+    public   String reverseWordsStatic(String s) {
+        String[] words = s.trim().split("\\s+");
+        StringBuilder reversed = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]).append(" ");
+        }
+        return reversed.toString().trim();
+    }
 }
