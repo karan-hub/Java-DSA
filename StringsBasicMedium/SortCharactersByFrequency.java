ea07d30248642c6eccc920915c7c4202d0f452af
karan <karanc4143@gmail.com>
2025-04-07 02:44:20 +0530

    🛠️ Modified tringsBasicMedium/SortCharactersByFrequency.java

--- StringsBasicMedium/SortCharactersByFrequency.java
+++ StringsBasicMedium/SortCharactersByFrequency.java
@@ -8,25 +8,37 @@ import java.util.Map;
 public class SortCharactersByFrequency {
     public static void main(String[] args) {
         SortCharactersByFrequency sorter = new SortCharactersByFrequency();
-        sorter.frequencySort("tree");
+    String result = sorter.frequencySort("tree");
+    System.out.println("Sorted by frequency: " + result);
     }
 
-    public void frequencySort(String s) {
-        List<Character>[] arr =  new ArrayList[s.length()+1];
+    public String frequencySort(String s) {
+        StringBuilder sb = new StringBuilder();
+        List<Character>[] arr = new ArrayList[s.length() + 1];
         Map<Character, Integer> map = new HashMap<>();
 
-    for (Character character : s.toCharArray()) map.put(character, map.getOrDefault(character, 0) + 1);
-            
-      System.out.println(map.toString());
+        for (Character character : s.toCharArray())
+            map.put(character, map.getOrDefault(character, 0) + 1);
 
-      map.keySet().forEach(
-        c ->{
-            if (arr[map.get(c)] == null) arr[map.get(c)] = new ArrayList<>();
-            
-            arr[map.get(c)].add(c);
+        System.out.println(map.toString());
+
+        map.keySet().forEach(
+                c -> {
+                    if (arr[map.get(c)] == null)
+                        arr[map.get(c)] = new ArrayList<>();
+                    arr[map.get(c)].add(c);
+                });
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            if (arr[i] != null) {
+                for (Character character : arr[i]) {
+                    for (int j = 0; j < i; j++) {
+                        sb.append(character);
+                    }
+                }
+            }
         }
-      );
-
+        return sb.toString();
 
     }
 }
