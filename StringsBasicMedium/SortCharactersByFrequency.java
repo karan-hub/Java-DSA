d0fece31635861f8523e03bf152f98ad9a1db8ac
karan <karanc4143@gmail.com>
2025-04-07 02:36:22 +0530

    🛠️ Modified tringsBasicMedium/SortCharactersByFrequency.java

--- StringsBasicMedium/SortCharactersByFrequency.java
+++ StringsBasicMedium/SortCharactersByFrequency.java
@@ -1,8 +1,32 @@
 package StringsBasicMedium;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class SortCharactersByFrequency {
     public static void main(String[] args) {
-        
+        SortCharactersByFrequency sorter = new SortCharactersByFrequency();
+        sorter.frequencySort("tree");
     }
-    
+
+    public void frequencySort(String s) {
+        List<Character>[] arr =  new ArrayList[s.length()+1];
+        Map<Character, Integer> map = new HashMap<>();
+
+    for (Character character : s.toCharArray()) map.put(character, map.getOrDefault(character, 0) + 1);
+            
+      System.out.println(map.toString());
+
+      map.keySet().forEach(
+        c ->{
+            if (arr[map.get(c)] == null) arr[map.get(c)] = new ArrayList<>();
+            
+            arr[map.get(c)].add(c);
+        }
+      );
+
+
+    }
 }
