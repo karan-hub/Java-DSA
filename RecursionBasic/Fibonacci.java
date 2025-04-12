901f12a0e77148b91a5dd01cf0292b7bdf51b22f
karan <karanc4143@gmail.com>
2025-04-13 03:00:25 +0530

    this is recursion  Recursive Palindrome Checker in Java

---
+++ RecursionBasic/Fibonacci.java
@@ -0,0 +1,14 @@
+package RecursionBasic;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(Series(4));
+
+    }
+
+    static  int Series(int n){
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return  Series(n-1)+ Series(n-2);
+    }
+}
