ccc005610922e995a21a02d87fd366b3d0967324
karan <karanc4143@gmail.com>
2025-04-19 01:10:16 +0530

    LinearSearch  int findIndex () complet by recursion

--- RecursionOnArrays/LinearSearch.java
+++ RecursionOnArrays/LinearSearch.java
@@ -2,13 +2,19 @@ package RecursionOnArrays;
 
 public class LinearSearch {
     public static void main(String[] args) {
-        System.out.println(find(new int[]{1,2,13,4,5}, 0 , 13 ));
+        System.out.println(findIndex(new int[]{1,2,13,4,5}, 0 , 13 ));
 
     }
 
     static  boolean find(int [] arr , int index , int target){
         if (index == arr.length-1) return false;
        return  arr[index] == target ||  find(arr ,++index ,target);
+
+    }
+    static  int findIndex(int [] arr , int index , int target){
+        if (index == arr.length-1) return -1;
+        else if (arr[index] == target) return  index;
+        else   return  findIndex(arr ,++index ,target);
 
     }
 }
