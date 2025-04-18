768b27c1e087d211807831bcf2d63ab45cfe9a2d
karan <karanc4143@gmail.com>
2025-04-19 02:12:03 +0530

    LinearSearch  ArrayList<Integer> ListAllIndex  () withought passing argument  complet by recursion

--- RecursionOnArrays/LinearSearch.java
+++ RecursionOnArrays/LinearSearch.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 
 public class LinearSearch {
     public static void main(String[] args) {
-        System.out.println(findIndex(new int[]{1,2,13,4,5}, 0 , 13 ));
-        ListAllIndex(new int[]{1,2,2 ,13,4,5}, 0 , 2 , new ArrayList<>() );
-        System.out.println(list);
+//        System.out.println(findIndex(new int[]{1,2,13,4,5}, 0 , 13 ));
+//        ListAllIndex(new int[]{1,2,2 ,13,4,5}, 0 , 2 , new ArrayList<>() );
+//        System.out.println(list);
+        System.out.println(ListAllIndex(new int[]{1,2, 2 ,13,4,5}, 0 , 2 ));
+
 
     }
 
@@ -33,4 +35,15 @@ public class LinearSearch {
         if (arr[index] == target)  result.add(index);
         return   ListAllIndex(arr ,++index ,target , list);
     }
+
+//    withought passing argument
+static   ArrayList<Integer> ListAllIndex(int [] arr , int index , int target ){
+        ArrayList<Integer> result = new ArrayList<>();
+    if (index == arr.length)  return  result ;
+//    this only for that call
+    if (arr[index] == target)   result.add(index);
+    ArrayList<Integer> fromCall =    ListAllIndex(arr ,++index ,target );
+    result.addAll(fromCall);
+    return  result;
+}
 }
