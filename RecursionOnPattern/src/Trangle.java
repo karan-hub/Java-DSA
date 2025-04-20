703fdb8475cc61ab77dbfa3b7b72e7b87dceea54
karan <karanc4143@gmail.com>
2025-04-20 22:54:31 +0530

    printTrangle using recursion

---
+++ RecursionOnPattern/src/Trangle.java
@@ -0,0 +1,28 @@
+public class Trangle {
+    public static void main(String[] args) {
+        printTrangle2(4,0);
+    }
+
+    static  void   printTrangle(int row , int col){
+        if (row ==0) return ;
+        if (col < row){
+            System.out.print(" * ");
+            printTrangle(row , col+1);
+        }else {
+            System.out.println();
+            printTrangle(row-1 , 0);
+        }
+    }
+
+
+    static  void   printTrangle2(int row , int col){
+        if (row ==0) return ;
+        if (col < row){
+            System.out.print(" " + (row +col) +" " );
+            printTrangle2(row , col+1);
+        }else {
+            System.out.println();
+            printTrangle2(row-1 , 0);
+        }
+    }
+}
