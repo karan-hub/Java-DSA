bb98fc9ed7f9ee9dacad08fb1c7133d2e11a8616
karan <karanc4143@gmail.com>
2025-04-27 23:43:42 +0530

    skipChar level 3

--- RecursionBasic/Skip.java
+++ RecursionBasic/Skip.java
@@ -2,7 +2,9 @@ package RecursionBasic;
 
 public class Skip {
     public static void main(String[] args) {
-        skipChar("ababababa" , "");
+//        skipChar("ababababa" , "");
+//        System.out.println(skipChar("karan"));
+        System.out.println(skipAppNotApple("If it's an imposter 'App', slice it off. If it's the true king 'Apple', crown it and move on."));
     }
 
     public static void   skipChar(String str , String result){
@@ -17,5 +19,22 @@ public class Skip {
         else skipChar(str.substring(1), result+ch);
 
     }
+
+    public static String   skipChar(String str ){
+        if (str.isEmpty()) return "";
+        if (str.startsWith("a")) return skipChar(str.substring(1));
+        else return  str.charAt(0)+skipChar(str.substring(1));
+    }
+
+    public static String   skipAppNotApple(String str ){
+        if (str.isEmpty())
+            return "";
+
+        if (str.startsWith("App") && !str.startsWith("Apple"))
+            return skipAppNotApple(str.substring(3));
+        else
+            return  str.charAt(0)+skipAppNotApple(str.substring(1));
+    }
+
 
 }
