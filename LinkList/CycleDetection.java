202bee2085cb43a99006b319ba5589a4c0946dc7
karan <karanc4143@gmail.com>
2025-06-28 17:19:38 +0530

    CycleDetection LL CycleLen Node

--- LinkList/CycleDetection.java
+++ LinkList/CycleDetection.java
@@ -14,26 +14,47 @@ public class CycleDetection {
             }
             return false ;
         }
+    public LL.Node detectCycle(LL.Node head) {
+        if (head == null || head.next == null) return null;
+
+        int len = CycleLen(head);
+        if (len == 0) return null;
+
+        LL.Node f = head;
+        LL.Node s = head;
+
+        while (len-- > 0) {
+            s = s.next;
+        }
+
+        while (f != s) {
+            f = f.next;
+            s = s.next;
+        }
+
+        return f;
+    }
     public int CycleLen(LL.Node head) {
-        LL.Node f  = head ;
-        LL.Node s  = head ;
-        int len= 1 ;
-        if(head == null) return 0 ;
-        if(head.next == null) return len ;
-        while(f !=  null && s  !=  null ){
-            if(f.next != null ) f = f.next.next;
-            else f = f.next;
-            s=s.next ;
-            if(f == s){
-                LL.Node p = f.next;
-                while (p != s){
-                    p =p.next;
+        LL.Node f = head;
+        LL.Node s = head;
+
+        while (f != null && f.next != null) {
+            f = f.next.next;
+            s = s.next;
+
+            if (f == s) {
+                int len = 1;
+                LL.Node temp = f.next;
+                while (temp != f) {
+                    temp = temp.next;
                     len++;
                 }
-                break;
+                return len;
             }
         }
-        return len ;
+
+        return 0;
     }
+
 
 }
