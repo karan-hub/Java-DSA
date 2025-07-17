8b2fb0ff720b957d8184270a99b0e3354417627b
karan <karanc4143@gmail.com>
2025-07-18 00:14:36 +0530

    StackUsingQueue

---
+++ Stack_And_Queue/StackUsingQueue.java
@@ -0,0 +1,29 @@
+package Stack_And_Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StackUsingQueue {
+
+    Queue<Integer> queue = new LinkedList<>();
+
+    public void push(int x) {
+        queue.add(x);
+        int size =queue.size();
+        while (size >1){
+            queue.add(queue.remove());
+            size--;
+        }
+    }
+    public int pop() {
+      return   queue.remove();
+    }
+    public int top() {
+        if (queue.isEmpty()) return 0;
+        return queue.peek(); // top element
+    }
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+
+}
