981583017967d97e2e059e1f0719f280248e66ee
karan <karanc4143@gmail.com>
2025-07-18 16:43:14 +0530

    NGE T1 T2

---
+++ Stack_And_Queue/NextGreater.java
@@ -0,0 +1,54 @@
+package Stack_And_Queue;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class NextGreater {
+    public int[] nextGreaterElement( int[] nums) {
+        int[] nge  = new int[nums.length];
+        Stack<Integer> stack =  new Stack<>();
+        for (int i = nums.length-1; i >=0 ; i--){
+            while (!stack.isEmpty() && stack.peek() <= nums[i])
+                stack.pop();
+            if (stack.isEmpty())
+                nge[i]= -1;
+            else
+                nge[i]=stack.peek();
+            stack.push(nums[i]);
+        }
+        return  nge;
+    }
+
+    public int[] nextGreaterElement2(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> ngeMap = new HashMap<>();
+
+        Stack<Integer> stack = new Stack<>();
+
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            int num = nums2[i];
+
+            while (!stack.isEmpty() && stack.peek() <= num) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) {
+                ngeMap.put(num, -1);
+            } else {
+                ngeMap.put(num, stack.peek());
+            }
+
+            stack.push(num);
+        }
+
+        // Step 2: Build result for nums1 from the map
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = ngeMap.get(nums1[i]);
+        }
+
+        return result;
+    }
+
+}
