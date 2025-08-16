3b2b73e1953a94ea7d8ff232a7dd7c60e5885422
karan <karanc4143@gmail.com>
2025-08-16 20:03:38 +0530

    Binary Search Tree completed

---
+++ BinaryTrees/BST.java
@@ -0,0 +1,93 @@
+package BinaryTrees;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class BST<T extends  Comparable<T> > {
+
+
+    private  static  class  Node<T>{
+         private final T value ;
+         private  Node<T> left;
+         private  Node<T> right;
+         public Node(T value) {
+             this.value = value;
+         }
+         public T getValue() {
+             return value;
+         }
+     }
+
+     private  Node<T> root;
+    public Node<T> getRoot() {
+        return root ;
+    }
+
+    public T getValue() {
+        return root.getValue() ;
+    }
+
+//     Insert in BST
+
+    public void insert(Scanner scanner ){
+        System.out.println("please Enter Root");
+        Integer value = scanner.nextInt();
+        root = insert((T) value, root);
+    }
+
+    public  Node<T> insert(T value , Node node){
+        if (node==null) return new Node<>(value);
+        if (value.compareTo((T) node.getValue()) <0) node.left = insert(value , node.left);
+        if (value.compareTo((T)node.getValue())>0) node.right = insert(value , node.right);
+        return node;
+    }
+    private  void Inorder(Node<T> node ,  String indent){
+        if (node == null )return;
+        Inorder(node.left , indent+"\t");
+        System.out.println(node.getValue());
+        Inorder(node.right , indent+"\t");
+    }
+
+    private void preOrder(Node<T> node , String indent) {
+        if (node ==null) return;
+        System.out.println(indent+ node.value);
+        preOrder(node.left,indent+"\t");
+        preOrder(node.right,indent+"\t");
+    }
+
+    private void postOrder(Node<T> node , String indent) {
+        if (node ==null) return;
+        preOrder(node.left,indent+"\t");
+        preOrder(node.right,indent+"\t");
+        System.out.println(indent+ node.value);
+    }
+
+    public void display(Scanner scanner) {
+        System.out.println("which order You Want \n prees 1 - PreOrder \n prees 2 - InOrder \n prees 3 - PostOrder");
+        int choice = scanner.nextInt();
+        switch (choice){
+            case 1 -> preOrder(this.root ," ");
+            case 2 -> Inorder(this.root," ");
+            case 3 -> postOrder(this.root," ");
+            default-> System.out.println("Invalid Entry For " + choice);
+        }
+    }
+
+    public static void main(String[] args) {
+        BST<Integer>  bst = new BST<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter u r Choice");
+        while (true){
+            int choice = scanner.nextInt();
+            switch (choice){
+               case  1 ->  bst.insert(scanner);
+               case  2 ->bst.display(scanner);
+                default -> {
+                    return;
+                }
+            }
+        }
+    }
+
+
+}
